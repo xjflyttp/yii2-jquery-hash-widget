3b4d71100b68d80721e1ee4af339bbc38b1e2f86
xjflyttp <xjflyttp@gmail.com>
2015-03-27 14:03:57 +0800

    init

---
+++ JqueryHashAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Hash Asset
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+
+namespace xj\jqueryhash;
+
+use yii\web\AssetBundle;
+
+class HashAsset extends AssetBundle {
+
+    public $sourcePath = '@bower/bower-jquery-hash';
+    public $js = ['jquery.hash.js'];
+    public $depends = ['yii\web\JqueryAsset'];
+
+}
